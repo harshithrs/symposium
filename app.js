e91f85a51afbc4fc0ca70d42e5613a162c19666e
ser-fuyah <hemanth18kumarv@gmail.com>
2019-10-23 17:44:14 +0530

    Idk

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ var express =require('express');
 var app = express();
 var path =require("path");
 
+
+app.use(express.static('public'));
+
+app.use('/fonts', express.static(__dirname + '/assests/fonts/font-awesome.min.css'));
+app.use('/css', express.static(__dirname + '/assests/css'));
+app.use('/images', express.static(__dirname + '/assests/img'));
+app.use('/js',express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/js',express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/css',express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 app.use(express.static('public'));
 
  
